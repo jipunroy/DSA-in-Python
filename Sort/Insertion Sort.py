b8e0980a2fd3e99f9de6e6843e5a1bef6d8a446c
jipunroy95 <jipunroy95@gmail.com>
2026-09-10 02:04:39 +0600

    insertion sort

---
+++ Sort/Insertion Sort.py
@@ -0,0 +1,19 @@
+# Insertion Sort function
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+
+arr = [7, 3, 8, 2, 6]
+
+print(insertion_sort(arr))
